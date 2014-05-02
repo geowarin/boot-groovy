8af73155f12e7f832a9af92fcb56ec34635bcab5
geowarin <geowarin@gmail.com>
2014-05-02 12:06:09 +0200

    In memory authentication

--- src/main/groovy/com/geowarin/SecurityConfig.groovy
+++ src/main/groovy/com/geowarin/SecurityConfig.groovy
@@ -1,7 +1,8 @@
 package com.geowarin
 
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Configuration
-import org.springframework.http.HttpMethod
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
 import org.springframework.security.config.annotation.web.servlet.configuration.EnableWebMvcSecurity
@@ -23,14 +24,17 @@ class SecurityConfig extends WebSecurityConfigurerAdapter {
         http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
         http.httpBasic()
         http.authorizeRequests()
-            .antMatchers('/management/**').authenticated()
-            .anyRequest().permitAll()
+                .antMatchers('/management/**').authenticated()
+                .anyRequest().permitAll()
     }
 
-//    @Override
-//    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-//        auth
-//                .inMemoryAuthentication()
-//                .withUser("user").password("password").roles("USER");
-//    }
+    @Value('${adminPassword:password}')
+    private String password
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth
+                .inMemoryAuthentication()
+                .withUser('admin').password(password).roles('ADMIN');
+    }
 }
--- src/test/groovy/AppIntegrationTests.groovy
+++ src/test/groovy/AppIntegrationTests.groovy
@@ -18,43 +18,42 @@ import org.springframework.web.client.RestTemplate
  * @author Geoffroy Warin (http://geowarin.github.io)
  */
 @RunWith(SpringJUnit4ClassRunner.class)
-@IntegrationTest(["port=0", "password=test"])
+@IntegrationTest(["port=0", "adminPassword=test"])
 @WebAppConfiguration
 @SpringApplicationConfiguration(classes = App.class)
 class AppIntegrationTests {
 
     @Value('${local.server.port}')
-    private int port;
-    @Value('${security.user.password}')
-    private String password;
-
-    String serverAddress
+    private int port
+    @Value('${adminPassword}')
+    private String password
+    private String serverAddress
 
     @Before
     public void setUp() throws Exception {
-        this.serverAddress = "http://localhost:${port}"
+        this.serverAddress = "http://localhost:$port"
     }
 
     @Test
     def void should_access_home() {
         def body = new RestTemplate().getForObject(serverAddress, String.class);
-        assert body == "Hello World!"
+        assert body == 'Hello World!'
     }
 
     @Test
     def void should_secure_sensible_resources() {
-        def httpResponse = Request.Get("${serverAddress}/management/beans").execute().returnResponse()
-        assert httpResponse.getStatusLine().getStatusCode() == 401
+        def httpResponse = Request.Get("$serverAddress/management/beans").execute().returnResponse()
+        assert httpResponse.statusLine.statusCode == 401
 
         assert password == 'test'
-        httpResponse = getWithBasicAuth("${serverAddress}/management/beans", 'admin', password)
-        assert httpResponse.getStatusLine().getStatusCode() == 200
+        httpResponse = getWithBasicAuth("$serverAddress/management/beans", 'admin', password)
+        assert httpResponse.statusLine.statusCode == 200
     }
 
     private static HttpResponse getWithBasicAuth(GString url, String userName, String password) {
-        def authorization = Base64.getEncoder().encodeToString((userName + ":" + password).getBytes());
+        def authorization = Base64.encoder.encodeToString("$userName:$password".bytes);
         Request.Get(url)
-                .addHeader("Authorization", "Basic " + authorization)
+                .addHeader('Authorization', "Basic $authorization")
                 .execute().returnResponse()
     }
 }
